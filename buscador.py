8728ea1cf95688ae4e0206da234082b3e4dbd91f
leandroAlves88 <lalves.ferreira88@gmail.com>
2023-09-20 11:03:47 -0300

    apresentação do retorno de resultado

--- buscador.py
+++ buscador.py
@@ -3,29 +3,15 @@ from bs4 import BeautifulSoup
 
 def motor_busca(campo_pesquisa):    
   cabecalho = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
-  url = 'https://www.pciconcursos.com.br/pesquisa/?q='+ campo_pesquisa
+  url = 'https://www.pciconcursos.com.br/pesquisa/?q='
 
-  site = requests.get(url, headers=cabecalho)
+  site = requests.get(url+campo_pesquisa, headers=cabecalho)
   soup = site.content
-
-#print(soup)
-#print(soup.prettify())
 
   soup = BeautifulSoup(site.content,'html.parser')
   vaga = soup.find("div", attrs = { "id":"concursos" })
-
-  link = vaga.find_all('a')
-  for linkvagas in link:
-      print(linkvagas.get('href'))
-
-  descricao = vaga.find_all('a')
-  # Imprima os links encontrados
-  for tituloVaga in descricao:
-      print(tituloVaga.get('title'))
-
-  imagens = vaga.find_all('img')
-  # Imprima os links encontrados
-  for imagensPrefeituras in imagens:
-      print(imagensPrefeituras.get('data-src'))
   
-  return ''
+  htmlVagas = vaga.find('div', attrs = {'class':'ca'})
+  print(htmlVagas.prettify())
+  
+  return vaga.prettify()
