f945ed26ed0e3bb0d75911d80c9241f382f2d6ee
leandroAlves88 <lalves.ferreira88@gmail.com>
2023-10-25 19:41:38 -0300

    Alteração de layout no front e mudança no motor de busca para retornar em formato Json

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import buscador
 import pandas as pd
 
@@ -18,18 +18,28 @@ def resultadosBusca():
     """
     Essa função tem o objetivo exibir a pagina dos resultados das buscas.
     """
+
     campo_busca = request.form["txtBusca"]
-
     print(f"campo pesquisar: {campo_busca}")
+    retorno_vagas = buscador.get_concursos_pci(campo_busca)
+
+    """retorno_vagas = pd.DataFrame()
+    print("Quantidade de registros iniciais:", len(retorno_vagas.index))
+
     retorno_vagas = buscador.motor_busca(campo_busca)
-    retorno_vagas = retorno_vagas.to_html(header="true", index=False, justify="center")
-    # return render_template('resultado.html',resultado = retorno_vagas)
-    return render_template("resultado.html", df_html=retorno_vagas, title="Vagas")
+    print(type(retorno_vagas))
+
+    print("Quantidade de registros encontrados:", len(retorno_vagas.index))
+
+    retorno_vagas = retorno_vagas.to_html(header="true", index=False, justify="center")"""
+
+    # return render_template("/resultados.html", df_html=retorno_vagas, title="Vagas")
+    return jsonify(retorno_vagas)
 
 
-@app.route("/resultado")
+@app.route("/resultados")
 def resultados():
-    return render_template("resultado.html")
+    return render_template("/resultados.html")
 
 
 @app.route("/login")
@@ -41,7 +51,7 @@ def login():
     # campo_senha = request.form["txtSenha"]
     # print(campo_login)
     # print(campo_senha)
-    return render_template("login.html")
+    return render_template("/login.html")
 
 
 @app.route("/sobre")
@@ -50,6 +60,14 @@ def sobre():
     Essa função tem o objetivo exibir a pagina de login.
     """
     return render_template("sobre.html")
+
+
+@app.route("/cadastro")
+def cadastro():
+    """
+    Essa função tem o objetivo exibir a pagina de login.
+    """
+    return render_template("cadastro.html")
 
 
 if __name__ == "__main__":
---
+++ static/javascript/script.js
@@ -0,0 +1,4 @@
+exportaRelatorio(){
+    const exportBtn = document.querySelector('class="btnExportar"');
+    console.log(exportBtn);
+}
